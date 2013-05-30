2420bf002d83288f7f0c30cbda4eff546284ba52
ileacristian85@gmail.com <ileacristian85@gmail.com@d1f06f37-437b-4fc7-70a7-637fba7288c4>
2013-05-30 19:54:41 +0000

    Add Read and Write function interfaces

--- Projects/DistanceSensor/inc/ds3231M_RTC.h
+++ Projects/DistanceSensor/inc/ds3231M_RTC.h
@@ -1,5 +1,8 @@
-#ifndef _DS3232RTC_H
-#define _DS3232RTC_H
+#ifndef _DS3231M_RTC_H
+#define _DS3231M_RTC_H
 
+u8 DS3231M_ReadByte(void);
+u8 DS3231M_ReadByteAdr(u8 _adr);
+void DS3231M_WriteBye(u8 _adr, u8 _data);
 
 #endif
