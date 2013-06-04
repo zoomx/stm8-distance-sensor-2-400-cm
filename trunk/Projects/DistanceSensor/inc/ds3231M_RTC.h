422907997f14a3af4e78ad84926dbc30dcc31f5d
ileacristian85@gmail.com <ileacristian85@gmail.com@d1f06f37-437b-4fc7-70a7-637fba7288c4>
2013-06-04 17:25:54 +0000

    add exported functions

--- trunk/Projects/DistanceSensor/inc/ds3231M_RTC.h
+++ trunk/Projects/DistanceSensor/inc/ds3231M_RTC.h
@@ -1,9 +1,11 @@
 #ifndef _DS3231M_RTC_H
 #define _DS3231M_RTC_H
 
-void DS3231M_Flush(void);
+void DS3231M_Flush(u8);
+u8 DS3231M_ReadByteAdr(u8);
 u8 DS3231M_ReadByte(void);
-u8 DS3231M_ReadByteAdr(u8 _adr);
-void DS3231M_WriteBye(u8 _adr, u8 _data);
+void DS3231_ReadBytes(u8, u8*, u8);
+void DS3231M_WriteByte(u8, u8);
+u8 DS3231M_WriteBytes(u8, u8*, u8);
 
 #endif
