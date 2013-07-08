426d3dc7a81bae7d2d21b47f202d18ce14330915
ileacristian85@gmail.com <ileacristian85@gmail.com@d1f06f37-437b-4fc7-70a7-637fba7288c4>
2013-07-08 15:50:04 +0000

    Flash Manager refinement - Needs testing!!

--- trunk/Projects/DistanceSensor/src/flashmngr.c
+++ trunk/Projects/DistanceSensor/src/flashmngr.c
@@ -6,8 +6,8 @@ For this purpose multiple continuous memory locations are used in SECTOR 0 for w
 When all these memory locations are occupied, the whole sector is erased and we start over.
 Data is meant to be stored as fixed lenght frame.
 */
-#define VALID                 (u8)1
-#define INVALID               (u8)0
+
+#define FLASH_UNLOCK_VAL      (u8)0x00
 #define PTR_ARR_SEC_ADR       (u32)0x000000   //SECTOR 0
 #define PTR_SEC_SIZE          (u16)4096       //SECTOR 0 size
 #define FLASH_MAX_ADR         (u32)0x1FFFFF   //MAX addressable flash memory location
@@ -23,41 +23,66 @@ Data is meant to be stored as fixed lenght frame.
 #define INVALID_ADR(_adr)     (((u32)_adr > FLASH_MAX_ADR) && ((u32)_adr < PTR_ERASED_VAL))   //macro to determine if _adr is an invalid flash memory address (different to the erased value)
 #define PTR_OUT_OF_ARR(_adr)  ( _adr >= (PTR_ARR_BASE + PTR_ARR_SIZE))                                     //macro to determine if _adr is out of PTR_ARR
 
-union uu
+union
 {
-  struct tt
+  struct
   {
     u8 PTR_ARR_Full      :1;   //set when PTR_ARR is full
-    u8 NeedErase         :1;   //set when invalid data is found in PTR_ARR
-    u8 UnknownERR        :1;   //CRITICAL: Unknown error
     u8 FlashPtr_was_Read :1;   //set when FlashMngr_GetPointer() function was called and valid flash pointer was returned
-    u8 FlashPtr_Invalid  :1;
     u8 Flash_Full        :1;   //set when Flash memory is full, no more data can be stored
     u8 Flash_was_Read    :1;   //set when Flash memory was Read, we assume that that we can erase the chip without data loss
-    u8 Flash_no_space    :1;   //set when FlashMngr_StoreData(u8*, u16) function ws called but there was not enough space to store the chunck of data
-    u8 PTR_SEC_Erase_ERR :1;   //CRITICAL: erase operation did not pass verification
-    u8 Flash_Erase_ERR   :1;   //CRITICAL: erase flash operation did not pass verification
-    u8 Data_Store_ERR    :1;   //data storage operation did not pass verification
+    u8 Flash_no_space    :1;   //set when FlashMngr_StoreData(u8*, u16) function was called but there was not enough space to store the chunk of data
+    u8 Operation_refused :1;   //operation refused by flash manager because the flash pointer was no read successfully or at all, or because of other errors
   }Status_bits;
-  u16 Status;
-}EXTFLASH;
+  u8 Status;
+}EXTFLASH_STAT;
+
+union
+{
+  struct
+  {
+    u8 Inconsistent_ERR     :1;   //set when invalid data is found in PTR_ARR
+    u8 Unknown_ERR          :1;   //CRITICAL: Unknown error
+    u8 Invalid_FlashPtr_ERR :1;   //CRITICAL: Invalid Flash pointer address
+    u8 PTR_SEC_Erase_ERR    :1;   //CRITICAL: erase operation did not pass verification
+    u8 Flash_Erase_ERR      :1;   //CRITICAL: erase flash operation did not pass verification
+    u8 Data_Store_ERR       :1;   //data storage operation did not pass verification
+    u8 Memory_Locked_ERR    :1;   //CRITICAL: memory patially or fully locked
+  }Error_bits;
+  u8 Error;
+}EXTFLASH_ERR;
 
 u8 buffer[PTR_SIZE];
-u8 flashimg[32];
 ExtFlashAdr flash_ptr;     //address of the next free flash memory location
 ExtFlashAdr pointer_adr;   //address of the next flash pointer in the flash pointer array
 
 _Bool FlashMngr_GetPointer(void);
 
+u8 FlashMngr_GetStatus()
+{
+  return EXTFLASH_STAT.Status;
+}
+
+u8 FlashMngr_GetErrors()
+{
+  return EXTFLASH_ERR.Error;
+}
+
 _Bool FlashMngr_Init()
 {
   _Bool tmp;
+  u8 tmpstatus; 
   ExtFlashAdr tmpadr;
-  tmpadr.adr32b = PTR_ARR_BASE;
   SST25VF016_Init();
-  SST25VF016_Read(tmpadr, flashimg, 32);
   tmp = FlashMngr_GetPointer();
-  SST25VF016_Write_Status_Register(0x00); //Unlock SPI flash memory for writing
+  //Unlock SPI flash memory for writing
+  SST25VF016_Write_Status_Register(FLASH_UNLOCK_VAL); 
+  //Check if memory was unlocked
+  if(SST25VF016_Read_Status_Register() & (STATUS_BP0 | STATUS_BP1 | STATUS_BP2))
+  {
+    EXTFLASH_ERR.Error_bits.Memory_Locked_ERR = 1;
+    return FALSE;
+  }
   flash_ptr.adr32b = DATA_STORAGE_BASE;
   pointer_adr.adr32b = PTR_ARR_BASE;
   return tmp;
@@ -85,19 +110,35 @@ _Bool FlashMngr_GetPointer()
   u32 ptr_old, ptr_new;
   u8 i;
   ExtFlashAdr tmpadr;
+  if(EXTFLASH_ERR.Error_bits.Inconsistent_ERR)
+  {
+    return FALSE;
+  }
   tmpadr.adr32b = PTR_ARR_BASE;
   ptr_old = FlashMngr_ReadPointer(tmpadr);
   if(INVALID_ADR(ptr_old))
   {
-    EXTFLASH.Status_bits.FlashPtr_Invalid = 1;
-    EXTFLASH.Status_bits.NeedErase = 1;
-    return INVALID;
+    EXTFLASH_ERR.Error_bits.Invalid_FlashPtr_ERR = 1;
+    EXTFLASH_ERR.Error_bits.Inconsistent_ERR = 1;
+    return FALSE;
   }
   if(ptr_old == PTR_ERASED_VAL)
   {
+    //Check if data storage space is empty
+    tmpadr.adr32b = DATA_STORAGE_BASE;
+    for(i = 0; i < TEST_ERASED_BYTES_NUM; i++)
+    {
+      if(SST25VF016_Read_Byte(tmpadr) != BYTE_ERASED_VAL) 
+      {
+        EXTFLASH_ERR.Error_bits.Inconsistent_ERR = 1;
+        return FALSE;
+      }
+      tmpadr.adr32b++;
+    }
     flash_ptr.adr32b = DATA_STORAGE_BASE;
     pointer_adr.adr32b = PTR_ARR_BASE;
-    return VALID;
+    EXTFLASH_STAT.Status_bits.FlashPtr_was_Read = 1;
+    return TRUE;
   }
   tmpadr.adr32b = PTR_ARR_BASE + PTR_SIZE;
   for(i = 1; i < PTR_ARR_LEN; i++)
@@ -105,121 +146,155 @@ _Bool FlashMngr_GetPointer()
     ptr_new = FlashMngr_ReadPointer(tmpadr);
     if((ptr_new <= ptr_old) || INVALID_ADR(ptr_new)) 
     {
-      EXTFLASH.Status_bits.FlashPtr_Invalid = 1;
-      EXTFLASH.Status_bits.NeedErase = 1;
-      return INVALID;
+      EXTFLASH_ERR.Error_bits.Invalid_FlashPtr_ERR = 1;
+      EXTFLASH_ERR.Error_bits.Inconsistent_ERR = 1;
+      return FALSE;
     }
     if(ptr_new == PTR_ERASED_VAL)
     {
       pointer_adr = tmpadr;    //pointing to the next erased location, flash pointer array not full, at least one free location
       flash_ptr.adr32b = ptr_old;
-      EXTFLASH.Status_bits.FlashPtr_was_Read = 1;
-      return VALID;
+      EXTFLASH_STAT.Status_bits.FlashPtr_was_Read = 1;
+      return TRUE;
     }
     tmpadr.adr32b += PTR_SIZE;
+    if((ptr_new > ptr_old) && (i == (PTR_ARR_LEN - 1)))
+    {
+      flash_ptr.adr32b = ptr_new;
+      EXTFLASH_STAT.Status_bits.PTR_ARR_Full = 1;
+      EXTFLASH_STAT.Status_bits.FlashPtr_was_Read = 1;
+      return TRUE;
+    }
     ptr_old = ptr_new;
   }
-  if(ptr_new > ptr_old)
-  {
-    flash_ptr.adr32b = ptr_new;
-    EXTFLASH.Status_bits.PTR_ARR_Full = 1;
-    EXTFLASH.Status_bits.FlashPtr_was_Read = 1;
-    return VALID;
-  }
-  EXTFLASH.Status_bits.UnknownERR = 1;
-  return INVALID;
+  //this is not a normal function exit
+  EXTFLASH_ERR.Error_bits.Unknown_ERR = 1;
+  EXTFLASH_ERR.Error_bits.Inconsistent_ERR = 1;
+  return FALSE;
 }
 
 _Bool FlashMngr_SavePointer(ExtFlashAdr adr)
 {
   u32 tmp;
   ExtFlashAdr tmpadr;
-  //if PTR_ARR full then erase PTR_ARR sector
-  if(EXTFLASH.Status_bits.PTR_ARR_Full)
+  if(EXTFLASH_ERR.Error_bits.Memory_Locked_ERR)
   {
-    tmpadr.adr32b = PTR_ARR_SEC_ADR;
-    SST25VF016_Sector_Erase_4KB(tmpadr);
-    //Wait until erase operation is completed
-    while(SST25VF016_Read_Status_Register() & STATUS_BUSY);
-    //Verify if the Sector was erased
-    tmpadr.adr32b = PTR_ARR_BASE;
-    if(FlashMngr_ReadPointer(tmpadr) != PTR_ERASED_VAL) 
+    //Operation refused, memory locked error
+    EXTFLASH_STAT.Status_bits.Operation_refused = 1;
+    return FALSE;
+  }
+  if(EXTFLASH_STAT.Status_bits.FlashPtr_was_Read)
+  {
+    EXTFLASH_STAT.Status_bits.Operation_refused = 0;
+    //if PTR_ARR full then erase PTR_ARR sector and set pointer_adr to PTR_ARR_BASE
+    if(EXTFLASH_STAT.Status_bits.PTR_ARR_Full)
     {
-      EXTFLASH.Status_bits.PTR_SEC_Erase_ERR = 1;
-      return FALSE;
+      tmpadr.adr32b = PTR_ARR_BASE;
+      SST25VF016_Sector_Erase_4KB(tmpadr);
+      //Wait until erase operation is completed
+      while(SST25VF016_Read_Status_Register() & STATUS_BUSY);
+      //Verify if the Sector was erased
+      if(FlashMngr_ReadPointer(tmpadr) != PTR_ERASED_VAL) 
+      {
+        EXTFLASH_ERR.Error_bits.PTR_SEC_Erase_ERR = 1;
+        EXTFLASH_ERR.Error_bits.Inconsistent_ERR = 1;
+        return FALSE;
+      }
+      EXTFLASH_STAT.Status_bits.PTR_ARR_Full = 0;
+      pointer_adr.adr32b = PTR_ARR_BASE;
     }
-    else EXTFLASH.Status_bits.PTR_SEC_Erase_ERR = 0;
-    EXTFLASH.Status_bits.PTR_ARR_Full = 0;
-    pointer_adr.adr32b = PTR_ARR_BASE;
+    //Write pointer to next free location
+    SST25VF016_Program_Byte(pointer_adr, adr.adr8b.HighByte);
+    pointer_adr.adr32b++;
+    SST25VF016_Program_Byte(pointer_adr, adr.adr8b.MidByte);
+    pointer_adr.adr32b++;
+    SST25VF016_Program_Byte(pointer_adr, adr.adr8b.LowByte);
+    pointer_adr.adr32b++;
+    //Set PTR_ARR_Full flag if array full
+    if(PTR_OUT_OF_ARR(pointer_adr.adr32b))
+    {
+      EXTFLASH_STAT.Status_bits.PTR_ARR_Full = 1;
+    }
+    //Verify what was written
+    tmpadr.adr32b = pointer_adr.adr32b - PTR_SIZE;
+    tmp = FlashMngr_ReadPointer(tmpadr);
+    if(tmp == adr.adr32b) return TRUE;
+    //if stored pointer is altered
+    EXTFLASH_ERR.Error_bits.Data_Store_ERR = 1;
+    EXTFLASH_ERR.Error_bits.Inconsistent_ERR = 1;
+    return FALSE;
   }
-  //Write pointer to next free location
-  SST25VF016_Program_Byte(pointer_adr, adr.adr8b.HighByte);
-  pointer_adr.adr32b++;
-  SST25VF016_Program_Byte(pointer_adr, adr.adr8b.MidByte);
-  pointer_adr.adr32b++;
-  SST25VF016_Program_Byte(pointer_adr, adr.adr8b.LowByte);
-  pointer_adr.adr32b++;
-  //Set PTR_ARR_Full flag if array full
-  if(PTR_OUT_OF_ARR(pointer_adr.adr32b))
-  {
-    EXTFLASH.Status_bits.PTR_ARR_Full = 1;
-  }
-  //Verify what was written
-  tmpadr.adr32b = pointer_adr.adr32b - PTR_SIZE;
-  tmp = FlashMngr_ReadPointer(tmpadr);
-  if(tmp == adr.adr32b) return TRUE;
+  //Operation refused by flash manager, no valid flash poiner was read, or flash pointer was not read at all
+  EXTFLASH_STAT.Status_bits.Operation_refused = 1;
   return FALSE;
 }
 
 _Bool FlashMngr_StoreData(u8* data, u16 size)
 {
   u8 i, tmp;
-  if(flash_ptr.adr32b + (size-1) > FLASH_MAX_ADR) 
+  if(EXTFLASH_ERR.Error_bits.Memory_Locked_ERR)
   {
-    EXTFLASH.Status_bits.Flash_no_space = 1;
+    //Operation refused, memory locked error
+    EXTFLASH_STAT.Status_bits.Operation_refused = 1;
     return FALSE;
   }
-  EXTFLASH.Status_bits.Flash_no_space = 0;
-  for(i = 0; i < size; i++)
+  if(EXTFLASH_STAT.Status_bits.FlashPtr_was_Read)
   {
-    SST25VF016_Program_Byte(flash_ptr, data[i]);
-    tmp = SST25VF016_Read_Byte(flash_ptr);
-    flash_ptr.adr32b++;
-    //if what was written wasn't as expected
-    if(tmp != data[i])
+    EXTFLASH_STAT.Status_bits.Operation_refused = 0;
+    //verify that we have enough free space in flash memory to write the requested data
+    if(flash_ptr.adr32b + (size-1) > FLASH_MAX_ADR) 
     {
-      EXTFLASH.Status_bits.Data_Store_ERR = 1;
-      FlashMngr_SavePointer(flash_ptr);
+      EXTFLASH_STAT.Status_bits.Flash_no_space = 1;
       return FALSE;
     }
+    EXTFLASH_STAT.Status_bits.Flash_no_space = 0;
+    for(i = 0; i < size; i++)
+    {
+      SST25VF016_Program_Byte(flash_ptr, data[i]);
+      //verify was was actually written
+      tmp = SST25VF016_Read_Byte(flash_ptr);
+      flash_ptr.adr32b++;
+      //if what was written wasn't as expected
+      if(tmp != data[i])
+      {
+        EXTFLASH_ERR.Error_bits.Data_Store_ERR = 1;
+        EXTFLASH_ERR.Error_bits.Inconsistent_ERR = 1;
+        return FALSE;
+      }
+    }
+    FlashMngr_SavePointer(flash_ptr);
+    return TRUE;
   }
-  EXTFLASH.Status_bits.Data_Store_ERR = 0;
-  FlashMngr_SavePointer(flash_ptr);
-  return TRUE;
+  //Operation refused by flash manager, no valid flash poiner was read, or flash pointer was not read at all
+  EXTFLASH_STAT.Status_bits.Operation_refused = 1;
+  return FALSE;
 }
 
 void FlashMngr_ReadData()
 {
-  EXTFLASH.Status_bits.Flash_was_Read = 1;
+  EXTFLASH_STAT.Status_bits.Flash_was_Read = 1;
 }
 
 _Bool FlashMngr_Erase_Chip()
 {
   u8 i, tmp;
   ExtFlashAdr tmpadr;
-  //if data in flash memory was read, then we can erase the chip
-  if(EXTFLASH.Status_bits.Flash_was_Read)
+  if(EXTFLASH_ERR.Error_bits.Memory_Locked_ERR)
   {
+    //Operation refused, memory locked error
+    EXTFLASH_STAT.Status_bits.Operation_refused = 1;
+    return FALSE;
+  }
+  //if data in flash memory was read, then we can erase the chip
+  if(EXTFLASH_STAT.Status_bits.Flash_was_Read)
+  {
+    EXTFLASH_STAT.Status_bits.Operation_refused = 0;
     SST25VF016_Chip_Erase();
-    EXTFLASH.Status_bits.PTR_ARR_Full = 0;
-    EXTFLASH.Status_bits.NeedErase = 0;
-    EXTFLASH.Status_bits.UnknownERR = 0;
-    EXTFLASH.Status_bits.FlashPtr_was_Read = 0;
-    EXTFLASH.Status_bits.Flash_Full = 0;
-    EXTFLASH.Status_bits.Flash_was_Read = 0;
-    EXTFLASH.Status_bits.Flash_no_space = 0;
+	  EXTFLASH_STAT.Status = 0;  //Reset status flags
+    EXTFLASH_ERR.Error = 0;    //Reset error flags
     flash_ptr.adr32b = DATA_STORAGE_BASE;
     pointer_adr.adr32b = PTR_ARR_BASE;
+	
     //Wait until chip erase is complete
     while(SST25VF016_Read_Status_Register() & STATUS_BUSY);
     //Verify if the chip was erased, test first TEST_ERASED_BYTES_NUM bytes starting from DATA_STORAGE_BASE
@@ -228,18 +303,28 @@ _Bool FlashMngr_Erase_Chip()
     {
       if(SST25VF016_Read_Byte(tmpadr) != BYTE_ERASED_VAL) 
       {
-        EXTFLASH.Status_bits.Flash_Erase_ERR = 1;
-        tmpadr.adr32b++;
+        EXTFLASH_ERR.Error_bits.Flash_Erase_ERR = 1;
+        EXTFLASH_ERR.Error_bits.Inconsistent_ERR = 1;
         return FALSE;
       }
+      tmpadr.adr32b++;
     }
-    EXTFLASH.Status_bits.Flash_Erase_ERR = 0;
+    tmpadr.adr32b = PTR_ARR_BASE;
+    //Test first pointer address from PTR_ARR_BASE (3 bytes)
+    if(FlashMngr_ReadPointer(tmpadr) != PTR_ERASED_VAL) 
+      {
+        EXTFLASH_ERR.Error_bits.PTR_SEC_Erase_ERR = 1;
+        EXTFLASH_ERR.Error_bits.Inconsistent_ERR = 1;
+        return FALSE;
+      }
+    //Flash pointer is known and valid
+    EXTFLASH_STAT.Status_bits.FlashPtr_was_Read = 1;
     return TRUE;
   }
+  //Operation refused, Flash was not Read before erase request
+  EXTFLASH_STAT.Status_bits.Operation_refused = 1;
   return FALSE;
 }
-
-
 
 u32 FlashMngr_Get_Free_Space()
 {
